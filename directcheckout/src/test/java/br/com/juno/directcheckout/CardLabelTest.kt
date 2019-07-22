5043adc508549fb0ee2e3ea61dd1a7a249075c78
Diefferson Santos <diefferson.sts@gmail.com>
2019-07-22 16:35:55 -0300

    Correção testes unitarios

--- directcheckout/src/test/java/br/com/juno/directcheckout/CardLabelTest.kt
+++ directcheckout/src/test/java/br/com/juno/directcheckout/CardLabelTest.kt
@@ -8,63 +8,63 @@ class CardLabelTest{
     @Test
     fun testVisaType(){
         CardsMock.visa.forEach {
-            assert(it.getType() is CardLabel.VISA)
+            assert(it.getType() == CardLabel.VISA.name)
         }
     }
 
     @Test
     fun testMasterCardType(){
         CardsMock.masterCard.forEach {
-            assert(it.getType() is CardLabel.MASTERCARD)
+            assert(it.getType() == CardLabel.MASTERCARD.name)
         }
     }
 
     @Test
     fun testAmexType(){
         CardsMock.amex.forEach {
-            assert(it.getType() is CardLabel.AMEX)
+            assert(it.getType() == CardLabel.AMEX.name)
         }
     }
 
     @Test
     fun testHiperCardType(){
         CardsMock.hipercard.forEach {
-            assert(it.getType() is CardLabel.HIPERCARD)
+            assert(it.getType() == CardLabel.HIPERCARD.name)
         }
     }
 
     @Test
     fun testDinersType(){
         CardsMock.diners.forEach {
-            assert(it.getType() is CardLabel.DINERS)
+            assert(it.getType() == CardLabel.DINERS.name)
         }
     }
 
     @Test
     fun testJcb15Type(){
         CardsMock.jcb15.forEach {
-            assert(it.getType() is CardLabel.JCB_15)
+            assert(it.getType() == CardLabel.JCB_15.name)
         }
     }
 
     @Test
     fun testJcb16Type(){
         CardsMock.jcb16.forEach {
-            assert(it.getType() is CardLabel.JCB_16)
+            assert(it.getType() == CardLabel.JCB_16.name)
         }
     }
 
     @Test
     fun testEloType(){
         CardsMock.elo.forEach {
-            assert(it.getType() is CardLabel.ELO)
+            assert(it.getType() == CardLabel.ELO.name)
         }
     }
 
     @Test
     fun testAuraType(){
         CardsMock.aura.forEach {
-            assert(it.getType() is CardLabel.AURA)
+            assert(it.getType() == CardLabel.AURA.name)
         }
     }
 
